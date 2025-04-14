802747ea7fadbab41bc61ee071d22e9e0eadd131
Shruti Khattri <shrutikhattri2003@gmail.com>
2025-04-14 22:16:04 +0530

    First commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
-export default function Home() {
+import React from 'react'
+
+export default function page() {
   return (
-    <></>
-  );
+    <div>
+      <p>This is a sample page</p>
+    </div>
+  )
 }
