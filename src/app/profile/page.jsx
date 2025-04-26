6ac30409cc7fc6889d86059b6dac426380722d29
unknown <subhashr898@gmail.com>
2025-04-26 13:10:48 +0530

    Profile page stated

---
+++ src/app/profile/page.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+export default function ProfileDashboardPage() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold mb-4 text-blue-700">Welcome to your Dashboard</h1>
+            <p className="text-gray-600">
+                Here you can view and manage your applications, update settings, and much more!
+            </p>
+        </div>
+    );
+}
